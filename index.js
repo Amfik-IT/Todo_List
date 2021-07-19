b1e8dd63ab8a9a5046735f34f5748ba96f87e238
Vladimir_Amfik <e.v.e.n@mail.ru>
2021-07-19 16:40:18 +0300

    localStorage

--- index.js
+++ index.js
@@ -1,10 +1,81 @@
 "use strict";
-const taskerApp = (function (){
+const taskerApp = (function () {
 
     class TaskerView {
 
-        constructor (container) {
+        constructor(container) {
+
             this.container = container;
+
+            this.HomePageComponent = {
+                id: "homepage",
+                title: "Tasker",
+                render: (className = "taskerPage", ...rest) => {
+                    return `
+                    <main class="${className}">
+                        <div class="head">
+                            <div class="head__day">${rest[0][0].day}</div>
+                            <button class="head__setupButton">...</button>
+                        </div>
+                        <div class="tasks"></div>
+                        <div class="lists"></div>
+                        <button class="addButton">+</button>
+                    </main>
+                  `;
+                }
+            };
+
+            this.router = {
+                homepage: this.HomePageComponent,
+                default: this.HomePageComponent,
+            };
+        }
+
+        init() {
+            return false;
+        }
+
+        renderContent(hashPageName, storageInfo) {
+            let routeName = "default";
+
+            if (hashPageName.length > 0) {
+                routeName = hashPageName in this.router ? hashPageName : "error";
+            }
+
+            window.document.title = this.router[routeName].title;
+            this.container.innerHTML = this.router[routeName].render(`${routeName}-page`, storageInfo);
+        }
+
+        createContent(storageInfo) {
+
+            let tasksList = document.createElement('ul');
+            tasksList.setAttribute("class", "tasks__list");
+            let tasks = storageInfo[0].tasks;
+            for (let i = 0; i < tasks.length; i++) {
+                let li = document.createElement('li');
+                li.innerHTML = tasks[i];
+                tasksList.append(li);
+            }
+            let divTasks = document.querySelector('.tasks');
+            divTasks.append(tasksList);
+
+            let listsList = document.createElement('ul');
+            listsList.setAttribute("class", "lists__list");
+            let lists = storageInfo[0].lists;
+            for (let i = 0; i < lists.length; i++) {
+                let li = document.createElement('li');
+                li.innerHTML = lists[i];
+                listsList.append(li);
+            }
+            let divLists = document.querySelector('.lists');
+            divLists.append(listsList);
+        }
+    };
+
+    class TaskerModel {
+
+        constructor(view) {
+            this.view = view;
         }
 
         init() {
@@ -12,44 +83,45 @@ const taskerApp = (function (){
         }
 
         firstLoad() {
-            this.container.innerHTML = "Hello"
+            let storageInfo = JSON.parse(window.localStorage.getItem("UserTaskInfo"));
+            this.updateState(storageInfo);
+            this.view.createContent(storageInfo);
         }
 
-    };
-
-    class TaskerModel {
-
-        constructor (view) {
-            this.view = view;
-        }
-
-        init () {
-            return false;
-        }
-
-        firstLoad() {
-            this.view.firstLoad();
+        updateState(storageInfo) {
+            const hashPageName = window.location.hash.slice(1).toLowerCase();
+            this.view.renderContent(hashPageName, storageInfo);
         }
     };
 
     class TaskerController {
 
-        constructor (model, container) {
+        constructor(model, container) {
             this.model = model;
             this.container = container;
         }
 
         init() {
+            if (window.localStorage.getItem("UserTaskInfo") === null) {
+                let storage = [{
+                    day: "Today",
+                    tasks: ["Start making a presentation", "Pay for rent", "Buy a milk", "Don’t forget to pick up Mickael from school", "Buy a chocolate for Charlotte"],
+                    lists: ["Inbox", "Work", "Shopping", "Family"],
+                }, ]
+                window.localStorage.setItem("UserTaskInfo", JSON.stringify(storage));
+            }
+
             this.firstLoad();
         }
 
         firstLoad() {
             this.model.firstLoad();
+            // this.model.updateState();
         }
     };
 
     return {
-        init: function() {
+        init: function () {
             const containerElem = document.getElementById("container");
 
             const appView = new TaskerView(containerElem);
