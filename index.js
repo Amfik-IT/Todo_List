a518e4fc0b36b6eb9105a918b60e9b54029ed48d
Vladimir_Amfik <e.v.e.n@mail.ru>
2021-07-19 13:58:46 +0300

    module_MVC

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-let a = 1;
+"use strict";
+const taskerApp = (function (){
+
+    class TaskerView {
+
+        constructor (container) {
+            this.container = container;
+        }
+
+        init() {
+            return false;
+        }
+
+        firstLoad() {
+            this.container.innerHTML = "Hello"
+        }
+
+    };
+
+    class TaskerModel {
+
+        constructor (view) {
+            this.view = view;
+        }
+
+        init () {
+            return false;
+        }
+
+        firstLoad() {
+            this.view.firstLoad();
+        }
+    };
+
+    class TaskerController {
+
+        constructor (model, container) {
+            this.model = model;
+            this.container = container;
+        }
+
+        init() {
+            this.firstLoad();
+        }
+
+        firstLoad() {
+            this.model.firstLoad();
+        }
+    };
+
+    return {
+        init: function() {
+            const containerElem = document.getElementById("container");
+
+            const appView = new TaskerView(containerElem);
+            const appModel = new TaskerModel(appView);
+            const appController = new TaskerController(appModel, containerElem);
+
+            appView.init();
+            appModel.init();
+            appController.init();
+        }
+    };
+})();
+
+document.addEventListener("DOMContentLoaded", taskerApp.init());
